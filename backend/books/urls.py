b6f3f9c5017c090c11f96f51b1b25fd7e7e37c1a
ChungRan <riplac0314@gmail.com>
2021-03-16 14:23:31 +0900

    book CRUD

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from books.views import BookAPI
+from books.views import BookListAPIView, BookDetailAPIView
 from django.contrib import admin
 from django.conf import settings
 
 
 urlpatterns = [
     # path('', BookPost.as_view(), name="bookPost"),
-    path('<int:id>', BookAPI.as_view(), name="bookAPI"),
+    path('book/', BookListAPIView.as_view(), name="bookAPI"),
+    path('book/<int:id>/', BookDetailAPIView.as_view(), name="bookAPIId"),
     # path('<int:id>/simple', BookSimple.as_view(), name="bookSimple"),
 ]
--- backend/books/views.py
+++ backend/books/views.py
@@ -8,7 +8,7 @@ from .serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-class BookAPI(APIView):
+class BookDetailAPIView(APIView):
     def get(self, request, id):
         # book = Book.objects.get(book_number = id)
         book = get_object_or_404(Book, book_number = id)
@@ -17,25 +17,42 @@ class BookAPI(APIView):
             'book' : bookJson
         }
         # return JsonResponse(returnJson)
-        return Response(returnJson)
-
-    def post(self, request, id):
-        #post는 다른 url로 옮기기
-        if request.META['CONTENT_TYPE'] == "application/json":
-            request = json.loads(request.body)
-            newBook = Book(title = request['title'],
-                           ISBN = request['ISBN'])
-        else:
-            newBook = Book(title = request.POST['title'],
-                           ISBN = request.POST['ISBN'])
-        newBook.save()
-        return HttpResponse(status=200)
+        return Response(data = returnJson)
 
     def put(self, request, id):
-        return
+        book = Book.objects.get(book_number = id)
+        serializer = BookSerializer(book, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(data = serializer.errors, status = 400)
 
     def delete(self, request, id):
+        # 작동안함
         book = Book.objects.get(book_number = id)
         book.delete()
-        return HttpResponse(status=200)
-    
+        print("디버깅 삭제 확인", id)
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+class BookListAPIView(APIView):
+    def get(self, request):
+        return
+
+    # def post(self, request):
+        #post는 다른 url로 옮기기
+        # if request.META['CONTENT_TYPE'] == "application/json":
+        #     request = json.loads(request.body)
+        #     newBook = Book(title = request['title'],
+        #                    ISBN = request['ISBN'])
+        # else:
+        #     newBook = Book(title = request.POST['title'],
+        #                    ISBN = request.POST['ISBN'])
+        # newBook.save()
+        # return HttpResponse(status=200)
+    def post(self, request):
+        serializer = BookSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data = serializer.data, status=201)
+        # print("디버깅", serializer.data)
+        return Response(data = serializer.errors, status = 400)

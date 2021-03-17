6a755162ad129a6efed961da7925d66fbe9f4773
ChungRan <riplac0314@gmail.com>
2021-03-17 23:47:03 +0900

    genre 수정

--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book, Author, Publisher, Genre, Country
+from .models import Book, Author, Publisher, Genre, GenreSub, Country
 
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Publisher)
 admin.site.register(Genre)
+admin.site.register(GenreSub)
 admin.site.register(Country)
